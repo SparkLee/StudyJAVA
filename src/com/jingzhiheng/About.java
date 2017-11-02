20482161c719cf98663f13b953e1d79bbd047a43
SparkLee <liweijsj@163.com>
2017-11-02 23:09:52 +0800

    添加src[存放java源代码]和web[存放资源文件]目录

---
+++ src/com/jingzhiheng/About.java
@@ -0,0 +1,19 @@
+package com.jingzhiheng;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "About", urlPatterns = {"/about", "/help/aboutus"})
+public class About extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>I'am Spark Lee, Welcome to my home page.</h1>");
+        out.flush();
+    }
+}
